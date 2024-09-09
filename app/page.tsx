0fa594f1948b49d0335f37f184e8c90c46b4d807
Sangram patil <133038961+thesandi99@users.noreply.github.com>
2024-09-09 17:07:48 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,19 @@
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="font-medium">
+    <div className="text-wrap font-bold p-10">
+      <main className="font-bold ">
         Memrinc is a secure, user-friendly platform that enables people to
         store, organize, and revisit their memories forever. Our platform allows
         users to upload photos, videos, and even written memories, all stored in
         a safe digital memory vault, accessible anytime, anywhere.{" "}
+        <div className="flex justify-center items-center mt-10">
+          <button className="bg-black rounded-md p-4 text-white">
+            Coming soon
+          </button>
+        </div>
+        <footer className="mt-6">
+          <span>Contact:hi@memrinc.com</span>
+        </footer>
       </main>
     </div>
   );
